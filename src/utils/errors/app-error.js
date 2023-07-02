d01463d994f937a43d21a1e0609df15f67b658a8
classr-22 <roundup414@gmail.com>
2023-07-02 12:47:04 +0530

    Added error files

---
+++ src/utils/errors/app-error.js
@@ -0,0 +1,18 @@
+const { StatusCodes } = require('http-status-codes');
+
+class AppError extends Error{
+    constructor(
+        name,
+        message,
+        explaination,
+        statusCodes
+    ){
+        super();
+        this.name = name,
+        this.message = message,
+        this.explaination = explaination,
+        this.statusCodes = statusCodes
+    }
+}
+
+module.exports = AppError;
